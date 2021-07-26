5f2c1a2d1b279f60afff7063427bf06b52cd3c7c
9831065 <najafizadeh21@gmail.com>
2021-07-26 11:18:43 +0430

    Refactor faild in MVC-1

--- 1. MVC Start/controllers/BaseController.php
+++ 1. MVC Start/controllers/BaseController.php
@@ -6,7 +6,6 @@ use app\core\Application;
 
 class BaseController
 {
-
     public function render($view, $params = []) {
         return Application::$app->router->renderView($view, $params);
     }
--- 1. MVC Start/controllers/LoginController.php
+++ 1. MVC Start/controllers/LoginController.php
@@ -3,16 +3,18 @@
 namespace app\controllers;
 
 use app\core\Request;
+use app\core\Application;
 
 class LoginController extends BaseController
 {
-
     public function handleLogin(Request $request) {
         $data = $request->getBody();
 
         if ($data['email'] == 'admin@admin.com' && $data['password'] == '1234') {
-            return $this->render('home', ['name' => 'admin']);
-            # TODO: Convert to redirect
+            $data = [
+                'name'=> 'Admin'
+            ];
+            return $this->render('home', $data);
         } else {
             return $this->render('login');
         }
