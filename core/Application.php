46929ed8198aed8baea79b5723e9cb7f63be58c7
9831065 <najafizadeh21@gmail.com>
2021-08-16 10:32:10 +0430

    Update application

--- core/Application.php
+++ core/Application.php
@@ -1,27 +1,77 @@
-<?php
+<?php 
 
-namespace app\core;
+namespace mvc\core;
 
-class Application
+use app\core\Request;
+use app\core\Response;
+use app\core\router\Router;
+use app\core\router\Routes;
+
+/**
+ * App class is the main class of our website, where
+ * we send user to different parts of website, or do the 
+ * website features.
+ * 
+ */
+class Application 
 {
-    public static string $ROOT_DIR;
-    public static Application $app;
-
-    public Request $request;
+    // App properties
+    public static App $app;
+    public static string $ROOT;
+    // App tools
     public Router $router;
+    public Request $request;
     public Response $response;
 
-    public function __construct(string $rootPath) {
-        self::$ROOT_DIR = $rootPath;
+    /**
+     * App constructor where we initialize the basics of the app
+     * object, like root directory, request, response, ...
+     * 
+     * @param root_dir the main directory of our project
+     */
+    private function __construct($root_dir)
+    {
         self::$app = $this;
+        self::$ROOT = $root_dir;
 
         $this->request = new Request();
         $this->response = new Response();
-
         $this->router = new Router($this->request, $this->response);
     }
 
-    public function run() {
-        echo $this->router->resolve();
+    /**
+     * Since we are using singleton pattern, we need to have a static method
+     * to create our app instance.
+     * 
+     * @param root_dir is the directory of our website
+     * @return app the created app instance
+     */
+    public static function get_instance($root_dir=NULL)
+    {
+        if (!isset(self::$app))
+        {
+            self::$app = new App($root_dir);
+            Routes::getRoutes();
+        }
+        return self::$app;
+    }
+
+    /**
+     * The run method, starts the application.
+     * 
+     */
+    public function run()
+    {
+        $result = $this->router->resolve();
+
+        if (is_array($result)) {
+            $this->response->setContentType('application/json');
+            echo json_encode($result, JSON_PRETTY_PRINT);
+            return;
+        }
+
+        echo $result;
     }
 }
+
+?>
